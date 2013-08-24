3b0ff794dac6f530dd39f8862b2f515015a104c2
jarllarsson <jarl.larsson@gmail.com>
2013-08-24 09:43:51 +0200

    Global time with rewinding and rewind cooldown.

---
+++ Assets/Scripts/ExistenceBuffer.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ExistenceBuffer : MonoBehaviour 
+{
+    private class TimeState
+    {
+        public Vector3 m_position;
+        public Quaternion m_rotation;
+        public Vector3 m_scale;
+    }
+
+    private List<TimeState> m_buffer;
+
+
+    // markers
+    private float m_startTime;
+    private float m_endTime;
+    private float m_currentDeltaTime;
+    // step size
+    private static float m_stepSizeSec;
+    private static float m_maxBufferSizeSec = 11.0f; // 10 seconds+security padding
+    // mode
+    private bool m_readingFromBuffer;
+
+
+	// Use this for initialization
+	void Start () 
+    {
+        m_startTime = GlobalTime.getTime();
+        //m_buffer = new List<TimeState>
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+    public bool isReadingFromBuffer()
+    {
+        return m_readingFromBuffer;
+    }
+
+
+
+}
--- Assets/Scripts/GlobalTime.cs
+++ Assets/Scripts/GlobalTime.cs
@@ -1,15 +1,149 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class GlobalTime : MonoBehaviour 
+{
+    public enum State
+    {
+        ADVANCING,
+        REWINDING
+    }
+
+    private static float m_time=0.0f;
+    private static bool m_locked = false;
+    private static State m_currentState = State.ADVANCING;
+
+    public float m_dbgTime = 0.0f;
+
+    private static float m_currentRewindAmountSec = 0.0f;
+
+    public bool m_dbgRewind = false;
+    public static float m_maxRewindTimeSec = 10.0f;
+    public static float m_rewindCooldownTimeSec = 0.0f;
+
+
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+    {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+    {
+        if (m_dbgRewind)
+        {
+            performRewind();
+        }
+        else
+        {
+            setRealtimeState();
+        }
+
+
+        if (!m_locked) advanceTime();
+        m_dbgTime = m_time;
 	}
+
+    private static bool performRewind()
+    {
+        bool success=false;
+        if (getCurrentCooldown() <= 0.0f &&
+            getCurrentRewindAmountSec() < m_maxRewindTimeSec)
+        {
+            m_currentState = State.REWINDING;
+            lockTime();
+            retardTime();
+            success=true;
+        }
+        else
+        {
+            setRealtimeState();
+        }
+        return success;
+    }
+
+    private static void setRealtimeState()
+    {
+        if (m_currentState == State.REWINDING)
+        {
+            m_rewindCooldownTimeSec = m_currentRewindAmountSec;
+            m_currentState = State.ADVANCING;
+            m_currentRewindAmountSec = 0.0f;
+            unlockTime();
+        }
+        cooldownRewind();
+    }
+
+    private static void cooldownRewind()
+    {
+        if (m_rewindCooldownTimeSec > 0.0f)
+        {
+            m_rewindCooldownTimeSec -= Time.deltaTime;
+        }
+        else
+        {
+            m_rewindCooldownTimeSec = 0.0f;
+        }
+    }
+
+    public static float getCurrentRewindAmountSec()
+    {
+        return m_currentRewindAmountSec;
+    }
+
+    public static float getCurrentRewindAmountPercent()
+    {
+        return m_currentRewindAmountSec / m_maxRewindTimeSec;
+    }
+
+    public static float getCurrentCooldown()
+    {
+        return m_rewindCooldownTimeSec;
+    }
+
+    public static float getCurrentCooldownPercent()
+    {
+        return m_rewindCooldownTimeSec / m_maxRewindTimeSec;
+    }
+
+    public static float advanceTime()
+    {
+        m_time += Time.deltaTime;
+        return m_time;
+    }
+
+    public static float retardTime() // ahahahahahah. tired
+    {
+        float oldTime = m_time;
+        m_time -= Time.deltaTime;
+        if (m_time < 0.0f)
+        {
+            m_time = 0.0f;
+        }
+        m_currentRewindAmountSec += oldTime - m_time;
+        return m_time;
+    }
+
+    public static State getState()
+    {
+        return m_currentState;
+    }
+
+    public static float getTime()
+    {
+        return m_time;
+    }
+
+    public static void lockTime()
+    {
+        m_locked = true;
+    }
+
+    public static void unlockTime()
+    {
+        m_locked = false;
+    }
+
 }
